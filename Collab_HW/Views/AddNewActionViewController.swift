782ec9960e3458fc4fa891d58359410f0aa8736e
Sergei <Goomicson@gmail.com>
2021-11-14 19:30:39 +0400

    Добавил нужные сториборды и сверстал интерфейс.

---
+++ Collab_HW/Views/AddNewActionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddNewActionViewController.swift
+//  Collab_HW
+//
+//  Created by Сергей on 14.11.2021.
+//
+
+import UIKit
+
+class AddNewActionViewController: UIViewController {
+
+    @IBOutlet var spendOrProfitSegmentedControl: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+
